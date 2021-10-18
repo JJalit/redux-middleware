cffdab040b00325ac87f7b87f61be911512ff1d8
jaeha <jaeha23@naver.com>
2021-10-18 09:24:12 +0900

    thunk

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import Counter from '../components/Counter';
-import { increase, decrease } from '../modules/counter';
+import { increaseAsync, decreaseAsync } from '../modules/counter';
 
 function CounterContainer() {
   const number = useSelector((state) => state.counter);
   const dispatch = useDispatch();
 
-  const onIncrease = () => dispatch(increase());
-  const onDecrease = () => dispatch(decrease());
+  const onIncrease = () => dispatch(increaseAsync());
+  const onDecrease = () => dispatch(decreaseAsync());
 
   return <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />;
 }
--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,10 +1,20 @@
-import React from 'react';
-
 const INCREASE = 'INCREASE';
 const DECREASE = 'DECREASE';
 
 export const increase = () => ({ type: INCREASE });
 export const decrease = () => ({ type: DECREASE });
+
+export const increaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(increase());
+  }, 1000);
+};
+
+export const decreaseAsync = () => (dispatch) => {
+  setTimeout(() => {
+    dispatch(decrease());
+  }, 1000);
+};
 
 const initialState = 0;
 
